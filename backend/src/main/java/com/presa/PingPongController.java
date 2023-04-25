a74a3223b979141fbd2a6a85a9ba380ee76727e8
Fernando <ivopresa@gmail.com>
2023-04-25 13:34:34 +0200

    CD workflow with new controller

--- backend/src/main/java/com/presa/PingPongController.java
+++ backend/src/main/java/com/presa/PingPongController.java
@@ -9,6 +9,6 @@ public class PingPongController {
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong");
+        return new PingPong("Pongggg");
     }
 }
