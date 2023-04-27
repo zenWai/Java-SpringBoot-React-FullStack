09cb5acffa0a4cee7dfec009f474b2f196d99066
Fernando <ivopresa@gmail.com>
2023-04-27 06:27:32 +0200

    Frontend + customer cards + gender feature on customer + tests fixed + more

---
+++ backend/src/main/java/com/presa/customer/Gender.java
@@ -0,0 +1,6 @@
+package com.presa.customer;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,67 @@
+import {
+    Wrap,
+    WrapItem,
+    Button,
+    Spinner,
+    Text } from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import { useEffect, useState } from "react";
+import {getCustomers} from "./Services/client.js";
+import CardWithImage from "./components/card.jsx";
+
+const App = () => {
+
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        setLoading(true);
+        getCustomers().then(res => {
+            setCustomers(res.data)
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    },[])
+
+    if (loading) {
+        return(
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+
+    if(customers.length<=0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+    }
+
+    return (
+        <SidebarWithHeader>
+            <Wrap justify={"center"} spacing={"30px"}>
+            {customers.map((customer, index) => (
+                <WrapItem>
+                    <CardWithImage
+                        {...customer}
+                        imageNumber={index}
+                    />
+
+                </WrapItem>
+            ))}
+            </Wrap>
+        </SidebarWithHeader>
+    )
+}
+export default App
