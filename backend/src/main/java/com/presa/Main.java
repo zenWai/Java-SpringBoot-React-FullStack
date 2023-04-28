8c4b376fcd1eca36e9648ac9cd8aa5ce9110d764
Fernando <ivopresa@gmail.com>
2023-04-28 18:43:13 +0200

    x

--- backend/src/main/java/com/presa/Main.java
+++ backend/src/main/java/com/presa/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
 
+
     }
     @Bean
     CommandLineRunner runner(CustomerRepository customerRepository) {
