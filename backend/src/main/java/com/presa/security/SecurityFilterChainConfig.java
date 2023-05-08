9d03f293bc96a265972fb5b358d6eea7502ef157
Fernando <ivopresa@gmail.com>
2023-05-08 19:51:20 +0200

    using bucket for website-images backend and frontend

--- backend/src/main/java/com/presa/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/presa/security/SecurityFilterChainConfig.java
@@ -13,7 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 @Configuration
 @EnableWebSecurity
